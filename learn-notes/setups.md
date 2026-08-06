542161526e76616efe8048a17312864af20b9904
stasshe <133995608+Stasshe@users.noreply.github.com>
2026-08-07 01:06:56 +0900

    basic loop without std

--- learn-notes/setups.md
+++ learn-notes/setups.md
@@ -1,4 +1,12 @@
 rustup target add x86_64-unknown-none
 
 
+sudo apt update
+sudo apt install -y qemu-system-x86 qemu-utils
+
+
+ABIってなんだ
+
+extern???
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// 渡し方を固定するらしいが、何に対してかわかってない
+// _startがCPUの最初のjump先。 
+// extern "C" でC言語の呼び出し規約(C ABI)に従う
+// Rustがfn nameをmangled nameに変換するが、
+// このときbootloaderは_startを探すので、no mangleで固定する
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        core::hint::spin_loop();
+    }
 }
