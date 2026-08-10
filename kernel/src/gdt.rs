ca73b66e7e3d6004d901b8ccebf4f1a68b7113b6
stasshe <133995608+Stasshe@users.noreply.github.com>
2026-08-11 01:25:44 +0900

    gdt SS fix

--- kernel/src/gdt.rs
+++ kernel/src/gdt.rs
@@ -36,12 +36,13 @@ lazy_static! {
 
 
 pub fn init() {
-    use x86_64::instructions::segmentation::{CS,Segment};
+    use x86_64::instructions::segmentation::{CS, SS, Segment};
     use x86_64::instructions::tables::load_tss;
 
     GDT.0.load();
     unsafe {
         CS::set_reg(GDT.1.code_selector);
+        SS::set_reg(x86_64::structures::gdt::SegmentSelector::NULL);
         load_tss(GDT.1.tss_selector);
     }
 }
